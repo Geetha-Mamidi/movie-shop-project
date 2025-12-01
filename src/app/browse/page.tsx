1d000cb62e7f848ae4e0183e44c8b45649c1f2b2
JollyRoger4u <petterssonroger@hotmail.com>
2025-12-01 10:32:27 +0100

    Navbar update
    fixing navigation links & adding rudimentary placeholder pages

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -18,3 +18,20 @@ Optinal:
 o Implement full-text search for movies using PostgreSQL's full-text search
 capabilities
 o Include search by director, actor, and genre*/
+export default function Dashboard() {
+  return (
+    <div className=" bg-zinc-50 font-sans dark:bg-black">
+      <main className="w-9/10 m-auto">
+        <h1 className="text-3xl font-bold mb-4">Start of movie content</h1>
+        <p className="text-muted-foreground">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique
+          provident a officiis, impedit deleniti odio tenetur id perferendis,
+          itaque aut veritatis consequuntur quae laboriosam in ullam tempora,
+          dolorum officia esse excepturi fuga. Possimus minus provident nesciunt
+          natus! Sed vel quam fuga, tempora magni totam consequatur reiciendis
+          provident asperiores, perspiciatis maiores.
+        </p>
+      </main>
+    </div>
+  );
+}
