2cbf4498179305d6da27de70d638f06e6a1f41e9
Geetha_Mamidi <geethamamidi13@gmail.com>
2026-01-07 09:38:01 +0100

    added wishlist button (#56)
    * added wishlist button
    
    * Adds wishlist back to dashboard sidebar
    
    ---------
    
    Co-authored-by: Geetha <mamidigeetha6@gmail.com>
    Co-authored-by: Light-Wave <66904273+Light-Wave@users.noreply.github.com>

--- src/components/layout/moviedetailview/detailed-movie-view.tsx
+++ src/components/layout/moviedetailview/detailed-movie-view.tsx
@@ -21,6 +21,7 @@ import { ArtistBadge } from "@/components/artist-badge";
 import AddToCartButton from "@/components/cartComponents/addToCartButton";
 import { Button } from "@/components/ui/button";
 import { Play } from "lucide-react";
+import AddToWishlistButton from "@/components/whishlistComponents/addToWishlistButton";
 import { StockRibbon } from "@/components/stock-ribbon";
 
 /**
@@ -184,7 +185,8 @@ export default function MovieDetailPage({
                 <div className="text-xl sm:text-2xl font-black text-green-700/90 italic">
                   <PriceDisplay price={movie.price} />
                 </div>
-                <div className="flex">
+                <div className="flex gap-2">
+                  <AddToWishlistButton movieId={movie.id} />
                   <AddToCartButton
                     disabled={!movie.isAvailable || (movie.stock ?? 0) === 0}
                     movieId={movie.id}
